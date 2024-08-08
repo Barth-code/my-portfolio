45dbc2931e47d7f26ac7ca30b5efaa0db9e724a4
Barth-code <bartaydev@gmail.com>
2024-08-08 07:03:37 -0400

    nav responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null); // Référence au menu burger
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    // Vérifie si le clic se fait en dehors du menu burger
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Ajoute un écouteur d'événement global pour les clics
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      // Nettoie l'écouteur d'événement lors du démontage du composant
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="navigation">
       <NavLink to="/">
         <img src="./logo.webp" alt="Ayena" />
       </NavLink>
-      <ul>
+      <div className="burger" onClick={toggleMenu}>
+        ☰
+      </div>
+      <ul ref={menuRef} className={`nav-links ${isOpen ? "open" : ""}`}>
         <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
           <li>Accueil</li>
         </NavLink>
@@ -15,7 +42,7 @@ const Navigation = () => {
           to="/compétances"
           className={(nav) => (nav.isActive ? "nav-active" : "")}
         >
-          <li>Compétance</li>
+          <li>Compétences</li>
         </NavLink>
         <NavLink
           to="/projets"
